ca980317e9fb7bc14576acb30d056f631342492d
ailingranados <ailin.granadoscn@uanl.edu.mx>
2025-08-06 18:38:23 -0600

    UI design
    Add of images and detects clicks on botton

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlet references a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //change the inicial number of the dice
+       
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button tapped")
+        
+        
+    }
+    
 }
 
